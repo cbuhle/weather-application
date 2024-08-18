c86f77e3dc01dd64938843682979ba47d5f0cf34
cbuhle <cbuhle3@gmail.com>
2024-08-18 23:23:42 +0200

    added classes and Id's

--- src/structure.js
+++ src/structure.js
@@ -1,7 +1,8 @@
 function submitCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#details");
+  let city = document.querySelector("h1");
+  city.innerHTML = searchInput.value;
 }
-let searchForm = document.querySelector("#details");
-console.log(searchForm.value);
+let searchForm = document.querySelector("#form");
 searchForm.addEventListener("submit", submitCity);
