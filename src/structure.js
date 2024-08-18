db8367c789514e316bc575fd43d312603f1bf9bf
cbuhle <cbuhle3@gmail.com>
2024-08-18 09:18:14 +0200

    making the search engine work

---
+++ src/structure.js
@@ -0,0 +1,7 @@
+function submitCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#details");
+}
+let searchForm = document.querySelector("#details");
+console.log(searchForm.value);
+searchForm.addEventListener("submit", submitCity);
