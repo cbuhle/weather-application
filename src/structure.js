2b3fe4a6b1d85a086e32cabdbbdbb8449d946b05
cbuhle <cbuhle3@gmail.com>
2024-08-20 16:59:54 +0200

    fetching my API

--- src/structure.js
+++ src/structure.js
@@ -1,3 +1,18 @@
+import axios from "axios";
+function updatePage(response) {
+  let temp = document.querySelector("#degrees");
+  let degrees = response.data.temperature.current;
+  temp.innerHTML = Math.round(degrees);
+  console.log(response.data.temperature.current);
+}
+
+function searchCity() {
+  let key = "34e269e9f408e8fa6ft73afaob64bf6c";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric";
+  axios.get(apiUrl).then(updatePage);
+}
+
 function submitCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#details");
